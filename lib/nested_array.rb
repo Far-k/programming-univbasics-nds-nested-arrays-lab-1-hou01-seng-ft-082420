5fb134507f52902ae179766acc2a610f8bb60b8d
Farzana Karim <farzana.mkarim@gmail.com>
2020-07-24 03:13:31 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -36,8 +36,8 @@ end
 def sorted_matrix
 
   [
-    CONVENTIONAL_PRODUCE.sort
-    ORGANIC_PRODUCE.sort,
+    CONVENTIONAL_PRODUCE.sort,
+    ORGANIC_PRODUCE.sort
 
   ]
   # Using Array literal syntax only, build another nested array that
